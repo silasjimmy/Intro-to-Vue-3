8b17451727e29b712a7e011d34a22088f33a1875
Silas Jimmy <jimmysilas17@gmail.com>
2023-11-11 04:15:28 +0300

    refactor: Solve lesson 3 problem

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: "./assets/images/socks_blue.jpg",
+            url: "https://github.com/silasjimmy/Intro-to-Vue-3"
         }
     }
 })
