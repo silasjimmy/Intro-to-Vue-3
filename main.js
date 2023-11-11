8b790f3524bd7016f489dbbc7502a249f3abea7e
Silas Jimmy <jimmysilas17@gmail.com>
2023-11-11 04:41:34 +0300

    Solve lesson 6 problem

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const app = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['S', 'M', 'L', 'XL'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
